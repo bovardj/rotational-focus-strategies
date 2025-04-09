2e2c29dc27fadc7acf1e35c14c2920f57d43e5a2
bovardj <bovardj@gmail.com>
2025-04-09 05:13:19 -0600

    Gave up on dynamic routing

---
+++ app/onboarding/strategies/environmental_shift/page.tsx
@@ -0,0 +1,29 @@
+import Breadcrumbs from '@/app/ui/strategies/breadcrumbs';
+// import { Metadata } from 'next';
+import { strategies } from '@/app/lib/utils';
+import StrategyDescriptions from '@/app/ui/dashboard/strategy-descriptions';
+
+export default async function Page() {
+  const strategy = 'environmental_shift';
+  const formattedStrategy = strategies.find(
+    (strat) => strat.href === strategy,
+  )?.name || strategy;
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Focus Strategies', href: '/dashboard/strategies' },
+          {
+            label: formattedStrategy,
+            href: `/dashboard/strategies/${strategy}/`,
+            active: true,
+          },
+        ]}
+      />
+      <div className="flex w-full items-center justify-between">
+        <StrategyDescriptions strategy={strategy} />
+      </div>
+    </main>
+  );
+}
