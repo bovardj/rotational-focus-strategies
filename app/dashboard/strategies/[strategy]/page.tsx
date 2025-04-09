a8b7c524ff477bb1263110a03975297ad9eb3af8
bovardj <bovardj@gmail.com>
2025-04-09 04:28:02 -0600

    Hopefully fixed the type error

--- app/dashboard/strategies/[strategy]/page.tsx
+++ app/dashboard/strategies/[strategy]/page.tsx
@@ -1,26 +1,39 @@
 import Breadcrumbs from '@/app/ui/strategies/breadcrumbs';
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 import { strategies } from '@/app/lib/utils';
 import StrategyDescriptions from '@/app/ui/dashboard/strategy-descriptions';
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ strategy: string }>;
-}): Promise<Metadata> {
-  const { strategy } = await params;
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: Promise<{ strategy: string }>;
+// }): Promise<Metadata> {
+//   const { strategy } = await params;
 
-  const formattedStrategy =
-    strategies.find((strat) => strat.href === strategy)?.name || strategy;
+//   const formattedStrategy =
+//     strategies.find((strat) => strat.href === strategy)?.name || strategy;
 
-  return {
-    title: `Strategies | ${formattedStrategy}`,
-    description: `Strategies | ${formattedStrategy}`,
-  };
-}
+//   return {
+//     title: `Strategies | ${formattedStrategy}`,
+//     description: `Strategies | ${formattedStrategy}`,
+//   };
+// }
 
-export default async function Page(props: { params: { strategy: string } }) {
-  const params = props.params;
+// export default async function Page({params}: { params: Promise<{ strategy: string }> }) {
+//   const { strategy } = await params;
+//   // const strategy = params.strategy;
+
+// export default async function Page(props: { params: Promise<{ strategy: string }> }) {
+//   const { strategy } = await props.params;
+  // const strategy = params.strategy;
+
+// export default async function Page(props: { params: Promise<{ strategy: string }> }) {
+//   const params = await props.params;
+//   const strategy = params.strategy;
+type StrategyParams = Promise<{ strategy: string }>;
+
+export default async function Page(props: { params: StrategyParams }) {
+  const params = await props.params;
   const strategy = params.strategy;
 
   const formattedStrategy = strategies.find(
