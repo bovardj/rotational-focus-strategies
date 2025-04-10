37c467c619ab20d435317605746f93237177cc14
bovardj <bovardj@gmail.com>
2025-04-09 18:40:10 -0600

    Improved formatting

--- app/ui/strategies/table-data.tsx
+++ app/ui/strategies/table-data.tsx
@@ -14,14 +14,14 @@ export default function TableData({ strategy }: { strategy: string }) {
       <Link
         href={link}
         className={clsx(
-          'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+          'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-md font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
           {
             'bg-sky-100 text-blue-600': pathname === link,
           },
         )}
       >
         <div className="w-2 h-2 rounded-full bg-black"></div>
-        <p className="text-s sm:text-sm md:block">{strategy}</p>
+        <p className="text-md md:block">{strategy}</p>
       </Link>
     </>
   );
