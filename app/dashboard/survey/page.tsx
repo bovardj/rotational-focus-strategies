02e3de9751d321d5f4da3cc43e8a90812793b183
bovardj <bovardj@gmail.com>
2025-04-13 16:22:43 -0600

    When all surveys are completed, including exit survey, the survey page displays a thank you.

--- app/dashboard/survey/page.tsx
+++ app/dashboard/survey/page.tsx
@@ -14,21 +14,40 @@ export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-2xl`}>
-        { endSurveyCompleted ? "End of Project Survey" :
-          `${ dailyCompleted ? "End of Project Survey" :
+        { endSurveyCompleted ? "You've completed all surveys! Thank you!" :
+          `${ dailyCompleted ? "Exit Survey" :
             `End of Day Survey ${ baselineCompleted ?
             " - Daily" : " - Baseline"}`
           }`
         }
         </h1>
-      <div className="flex justify-center">
+        { endSurveyCompleted ? (
+          <p className="mb-4 ml-4">
+            Thank you for helping me with my project. You have completed all surveys.
+          </p>
+        ) : (
+          <>
+          <p className="mb-4 ml-4">
+            Please complete the survey below.
+          </p>
+          <div className="flex justify-center">
+          <div className="grid gap-6 sm:grid-cols-1 lg:grid-cols-2">
+            <SurveyForm
+              dailyCompleted={dailyCompleted}
+              baselineCompleted={baselineCompleted}
+            />
+          </div>
+        </div>
+        </>
+        )}
+      {/* <div className="flex justify-center">
         <div className="grid gap-6 sm:grid-cols-1 lg:grid-cols-2">
           <SurveyForm
             dailyCompleted={dailyCompleted}
             baselineCompleted={baselineCompleted}
           />
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
