9cda2d1eb7db6e4d1dbff6d11d078885537eb229
bovardj <bovardj@gmail.com>
2025-04-14 11:30:56 -0600

    Added a 'Last Day' accordion with Exit Survey info and the data handling policy, added 'Two Important Notes', and made formatting more consistent.

--- app/ui/dashboard/components/collapse-instructions.tsx
+++ app/ui/dashboard/components/collapse-instructions.tsx
@@ -27,16 +27,33 @@ export default function CollapseInstructions(
             You only need to select what day the survey is for in the survey. Please try to complete the
             survey as soon as possible after the end of each day, though.
           </p>
-          <p>
-            Please note that new focus strategies are assigned at 1am MDT (3am EDT) each day.
+          <p className="italic underline">
+            <b>Two Important Notes:</b>
+          </p>
+          <ul className='list-disc pl-6'>
+            <li>
+              While you will only be assigned Focus Strategies from your selection, it is not guaranteed that 
+              you will be assigned all of your focus strategies. Additionally, it is possible that you will be
+              assigned the same Focus Strategy two days in a row.
+            </li>
+            <li>
+              Please note that new focus strategies are assigned at 1am MDT (3am EDT) each day.
+            </li>
+          </ul>
+          {/* NOTE TO SELF: This is where the PWA installation instructions are. Remove "hidden" when PWA functionality is implemented. */}
+          <p className="hidden mb-4">
+            While completely optional, you may install this app onto your device, such as a smartphone or computer,
+            as a Progressive Web App (PWA) if you would like. A useful guide to installing the app as a PWA can be found&nbsp;
+            <Link href="https://www.cdc.gov/niosh/mining/tools/installpwa.html" className="text-blue-500 hover:underline">
+            here</Link>.
           </p>
           <Collapse shadow title={`Baseline Days (first ${baselineSurveysExpected} days)`} className='bg-gray-50'>
           <ul className="list-disc pl-6">
               <li className="mb-2">
                 Go about your day as you normally would.
               </li>
-              <li className="mb-2">
-                At the end of the day, complete the <Link href='./dashboard/survey' className='underline'>survey</Link> about your day.
+              <li>
+                At the end of the day, complete a Baseline <Link href='./dashboard/survey' className='underline'>Survey</Link> about your day.
                 <span className='italic'> (Also accessible from the &apos;End of Day Survey&apos; button on the dashboard.)</span>
               </li>
           </ul>
@@ -47,11 +64,32 @@ export default function CollapseInstructions(
                 Use your assigned focus strategy for the day.
               </li>
               <li className="mb-2">
-                At the end of the day, complete the <Link href='./dashboard/survey' className='underline'>survey</Link> about your experience with that strategy.
+                At the end of the day, complete the Daily <Link href='./dashboard/survey' className='underline'>
+                Survey</Link> about your experience with that strategy.
                 <span className='italic'> (Also accessible from the &apos;End of Day Survey&apos; button on the dashboard.)</span>
               </li>
-              <li className="mb-2">
-                When you submit your last end of day survey, you will be directed to a brief exit <Link href='./dashboard/survey' className='underline'>survey</Link>. 
+              <li>
+                When you submit your last Daily (post-Basline) Survey, you will be redirected to a brief
+                Exit <Link href='./dashboard/survey' className='underline'>Survey</Link>. 
+              </li>
+          </ul>
+          </Collapse>
+          <Collapse shadow title='Last Day' className='bg-gray-50'>
+          <ul className="list-disc pl-6">
+              <li className='mb-2'>
+                When you submit your last Daily (post-Basline) Survey, you will be redirected to a brief
+                Exit <Link href='./dashboard/survey' className='underline'>Survey</Link>.
+              </li>
+              <li>
+                <p className='mb-2'>
+                  I will delete all of your data from the apps database within one week of your Exit Survey submission.
+                  Associations to your email address or other potentially identifying information will be deleted and 
+                  data transfered to an anonymized spreadsheet for analysis and reporting purposes.
+                </p>
+                <p>
+                  While you can continue to use the app and get assigned strategies after your last survey, this will
+                  no longer be the case once I delete your data from the database.
+                </p>
               </li>
           </ul>
           </Collapse>
