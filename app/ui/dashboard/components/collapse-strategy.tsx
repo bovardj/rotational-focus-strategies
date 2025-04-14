f3b90de0846c3af20f009edf31c1563dd7df4a02
bovardj <bovardj@gmail.com>
2025-04-13 19:18:28 -0600

    Fixed a formatting error

--- app/ui/dashboard/components/collapse-strategy.tsx
+++ app/ui/dashboard/components/collapse-strategy.tsx
@@ -7,7 +7,7 @@ export default function CollapseStrategy({ strategyList: strategyList }: { strat
 
     return (
         <>
-            <div className="flex items-center sm:w-2/3 lg:w-1/2">
+            <div className="grid gap-6 grid-cols-1 md:w-3/4 lg:w-2/3 xl:w-1/2">
                 <Collapse shadow title="Your Focus Strategies" className='bg-gray-50'>
                     {strategyList.map((strategy, index) => (
                         <div key={index} className="flex items-center">
