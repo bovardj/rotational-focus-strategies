889e33ff3da02814994ade5cf17d87fe8083279c
bovardj <bovardj@gmail.com>
2025-04-18 13:09:50 -0600

    Added PWA and Notifications info to "Notes" collapse

--- app/ui/dashboard/components/collapse-notes.tsx
+++ app/ui/dashboard/components/collapse-notes.tsx
@@ -10,6 +10,26 @@ export default function CollapseNotes() {
         <Collapse shadow title="Notes and Known Issues" className='bg-gray-50'>
         <div>
           <p className="italic underline mb-4 text-md">
+            Install as a Progressive Web App
+          </p>
+          <p className="mb-4 text-md">
+            If you would like to install this as a Progressive Web App (PWA), you can do so by
+          </p>
+          <ul className="list-disc list-inside mb-4 text-md">
+            <li>
+              <b>iOS:</b> click the share button in Safari and select &quot;Add to Home Screen&quot;
+            </li>
+            <li>
+              <b>Android:</b> click the three dots in the top right corner of Chrome and select &quot;Install App&quot;
+              (I believe Firefox is the same)
+            </li>
+            <li>
+              <b>Further information:</b> A useful guide to installing the app as a PWA can be found&nbsp;
+              <Link href="https://www.cdc.gov/niosh/mining/tools/installpwa.html" className="text-blue-500 hover:underline">
+              here</Link>.
+            </li>
+          </ul>
+          <p className="italic underline mb-4 text-md">
             Looking for the log out button?
           </p>
           <p className="mb-4 text-md">
@@ -20,6 +40,12 @@ export default function CollapseNotes() {
           </p>
           <p className="mb-4 text-md">
             This app may be a little slow--I apologize for that. I am working on improving the performance of the app.
+          </p>
+          <p className="italic underline mb-4 text-md">
+            Want notifications?
+          </p>
+          <p className="mb-4 text-md">
+            Unfortunately, notifications are not supported at this time.
           </p>
           <p className="italic underline mb-4 text-md">
             Multiple strategies assigned in a single day?
