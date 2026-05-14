e242c4ed8832fc153b9bf03ee86362773441aa27
bovardj <bovardj@gmail.com>
2026-05-14 16:01:24 -0600

    chore: update Supabase client initialization to use PUBLISHABLE_KEY instead of ANON_KEY

--- app/lib/actions/notifications.ts
+++ app/lib/actions/notifications.ts
@@ -6,7 +6,7 @@ import webpush from 'web-push'
 
 const supabase = createClient(
   process.env.SUPABASE_URL || '',
-  process.env.SUPABASE_ANON_KEY || '',
+  process.env.SUPABASE_PUBLISHABLE_KEY || '',
   {
     async accessToken() {
       return (await auth()).getToken()
