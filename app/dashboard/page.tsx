97847400e48150e4b9969add6b7c95d98fc6ccfa
bovardj <bovardj@gmail.com>
2026-06-29 18:57:09 -0600

    Fixed dashboard card layout for medium size screens

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -102,7 +102,7 @@ export default async function Page() {
           dailySurveysExpected={dailySurveysExpected}
           endSurveyCompleted={endSurveyCompleted}
         />
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
           <CollapseStrategy strategyList={strategyList} />
           <CollapsePreviousStrategy
             previousStrategyList={filteredAssignedStrategies}
